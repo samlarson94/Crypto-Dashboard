f7e9b4b672887677a27c01ac4cc6e068755f464b
Imkubes <99090378+Imkubes@users.noreply.github.com>
2022-05-27 13:48:53 -0500

    saving changes

--- client/src/components/profile/Profile.jsx
+++ client/src/components/profile/Profile.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 const Profile = () => {
   return (
     <section id='profile'>
-
+    <div className='container profile__container'>
+        
+    </div>
     </section>
   )
 }
