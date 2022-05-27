1827ac9eb1f89ef48c342942c1e19caadfb8907c
Imkubes <99090378+Imkubes@users.noreply.github.com>
2022-05-27 14:32:51 -0500

    profile component started

--- client/src/components/profile/Profile.jsx
+++ client/src/components/profile/Profile.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
+import './profile.css'
 
 const Profile = () => {
-  return (
-    <section id='profile'>
-    <div className='container profile__container'>
-        
-    </div>
-    </section>
-  )
+    return (
+        <section className='profile__title' id='profile'>
+            <h1>Welcome to your Crypto Dashboard User...</h1>
+            <div className='container profile__container'>
+
+            </div>
+        </section>
+    )
 }
 
 export default Profile
