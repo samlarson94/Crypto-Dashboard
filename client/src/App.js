947bc1af49abbf78cd2549c5211515364170fd2f
Imkubes <99090378+Imkubes@users.noreply.github.com>
2022-05-27 10:55:05 -0500

    react app creation

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,25 @@
-// import React from 'react';
-// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import logo from './logo.svg';
+import './App.css';
 
-// REFERENCE CODE
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
 
-// function App() {
-//   return (
-//     <Router>
-//       <div className="flex-column justify-center align-center min-100-vh bg-primary">
-//         <Routes>
-//           <Route 
-//             path="/" 
-//             element={<Home />} 
-//           />
-//           <Route 
-//             path="/matchup" 
-//             element={<Matchup />} 
-//           />
-//           <Route 
-//             path="/matchup/:id" 
-//             element={<Vote />} 
-//           />
-//         </Routes>
-//       </div>
-//     </Router>
-//   );
-// }
-
-// export default App;
+export default App;
