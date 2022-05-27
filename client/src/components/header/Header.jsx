924c65b6c12e2533816cab18d1cba562222263e5
Imkubes <99090378+Imkubes@users.noreply.github.com>
2022-05-27 12:43:47 -0500

    saving homepage changes

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,16 +1,20 @@
 import React from 'react'
 import './header.css'
 import homeIMG from '../../assets/home_image.png'
+import Login from './Login'
 
 const Header = () => {
   return (
-    <header className='container header__container'>
-        <h1>Welcome to the Crypto Tracker!</h1>
+    <header>
+    <div className='container header__container'>
+    <h1>Welcome to the Crypto Tracker!</h1>
         <h5>Here you can track your favorite coins!</h5>
-
+        <Login />
+        
         <div className='homeImage'>
             <img src={homeIMG} alt='homeimage' />
         </div>
+    </div>
     </header>
   )
 }
