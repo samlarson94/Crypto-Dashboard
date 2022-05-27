8b0fbf36294e740fbc9bdfd0f713c2b865a75481
Brennan <brennanheley@gmail.com>
2022-05-27 14:51:49 -0500

    added seed and crypto model

--- server/seeds/seed.js
+++ server/seeds/seed.js
@@ -3,8 +3,8 @@ const { Crypto } = require("../models");
 const cryptoSeeds = require("./cryptoSeeds.json");
 
 db.once("open", async () => {
-  // await Crypto.deleteMany({});
-  await Crypto.create(cryptoSeeds);
+  await Crypto.deleteMany({});
+  await Crypto.insertMany(cryptoSeeds);
 
   console.log("all done!");
   process.exit(0);
