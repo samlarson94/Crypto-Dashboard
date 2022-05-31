d52e52310c1fa37aa8027c87d443a0b6d8f50784
samlarson94 <samlarson94@gmail.com>
2022-05-31 13:03:53 -0500

    pushing to heroku

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,9 @@ import React from 'react'
 import { ApolloProvider } from '@apollo/client';
 import { ApolloClient } from '@apollo/client';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from './pages/home'
+import Home from './pages/blah/home'
 import Login from './pages/login'
-import Signup from './pages/signup'
+import Signup from './pages/Signup'
 import Nav from './components/nav/Nav'
 import Profile from './components/profile/Profile'
 import Footer from './components/footer';
