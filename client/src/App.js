86752008513005fe1ebaf67274cb588194abd082
Imkubes <99090378+Imkubes@users.noreply.github.com>
2022-05-31 11:38:44 -0500

    saving changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,25 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import home from './pages/home'
-import login from './pages/login'
-import signup from './pages/signup'
+import Home from './pages/home/Home'
+import Login from './pages/Login'
+import Signup from './pages/Signup'
 import Header from './components/header/Header'
 import Nav from './components/nav/Nav'
 import Profile from './components/profile/Profile'
+import { ApolloProvider } from '@apollo/client';
+import { ApolloClient } from '@apollo/client';
 
 
 const App = () => {
   return (
-    <>
-        <Header />
-        <Nav />
-        <Profile />
-    </>
+    <ApolloProvider client={ApolloClient}>
+      <Router>
+          <Nav />
+          <Routes>
+            <Route path='/' element={<Home />} />
+          </Routes>
+      </Router>
+    </ApolloProvider>
   )
 }
 
--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
-import '../components/header/header.css'
-import homeIMG from '../assets/home_image.png'
-import Login from './login'
+import '../home/home.css'
+import homeIMG from '../../assets/home_image.png'
+import Login from '../Login'
 
 const Home = () => {
   return (
-    <header>
     <div className='container header__container'>
     <h1>Welcome to the Crypto Tracker!</h1>
         <h5>Here you can track your favorite coins!</h5>
@@ -15,7 +14,6 @@ const Home = () => {
             <img src={homeIMG} alt='homeimage' />
         </div>
     </div>
-    </header>
   )
 }
 
