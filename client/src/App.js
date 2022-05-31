8b2757efe4797e0a4b1040f038b0f6d6682773b1
Imkubes <99090378+Imkubes@users.noreply.github.com>
2022-05-31 13:43:03 -0500

    homepage

--- client/src/App.js
+++ client/src/App.js
@@ -2,23 +2,12 @@ import React from 'react'
 import { ApolloProvider } from '@apollo/client';
 import { ApolloClient } from '@apollo/client';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-<<<<<<< HEAD
 import Home from './pages/home/Home'
-import Login from './pages/Login'
+import Login from './pages/Loginpage'
 import Signup from './pages/Signup'
-import Header from './components/header/Header'
-import Nav from './components/nav/Nav'
-import Profile from './components/profile/Profile'
-import { ApolloProvider } from '@apollo/client';
-import { ApolloClient } from '@apollo/client';
-=======
-import Home from './pages/home'
-import Login from './pages/login'
-import Signup from './pages/signup'
 import Nav from './components/nav/Nav'
 import Profile from './components/profile/Profile'
 import Footer from './components/footer';
->>>>>>> main
 
 
 
@@ -27,15 +16,6 @@ function App() {
   return (
     <ApolloProvider client={ApolloClient}>
       <Router>
-<<<<<<< HEAD
-          <Nav />
-          <Routes>
-            <Route path='/' element={<Home />} />
-          </Routes>
-      </Router>
-    </ApolloProvider>
-  )
-=======
             <Nav />
             <Routes>
               <Route path ="/" element={<Home />} />
@@ -47,7 +27,6 @@ function App() {
       </Router>
     </ApolloProvider>
   );
->>>>>>> main
 }
 
 export default App
--- client/src/pages/home/Login.jsx
+++ client/src/pages/home/Login.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import '../home/home.css'
 
 const Login = () => {
   return (
