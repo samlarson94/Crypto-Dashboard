b4cb1e24a52c1dcd3bebbab87f566ccbc5360e2f
klmitchy <karenapolis@gmail.com>
2022-06-03 11:24:42 -0500

    profile page

--- client/src/pages/profile/Profile.jsx
+++ client/src/pages/profile/Profile.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import './profile.css'
 import Nav from '../../components/nav/Nav'
+//import CoinList from '../../components/coinInfo/CoinList'
+import FavoriteList from '../components/FavoriteList'
+import { QUERY_USER, QUERY_FAVORITES } from '../utils/queries';
 
 import Auth from '../utils/auth';
 
@@ -33,12 +36,44 @@ const Profile = () => {
   return (
     <>
     <Nav />
-    <div className='container profile__container'>
-    <h1>Your Crypto Tracker!</h1>
-        <h5>Here are your favorite coins!</h5>
-    </div>
-    </header>
-  )
-}
+    <div>
+      <div className="flex-row justify-center mb-3">
+        <h2 className="col-12 col-md-10 bg-dark text-light p-3 mb-5">
+          Viewing {userParam ? `${user.username}'s` : 'your'} profile.
+        </h2>
+
+        <div className="col-12 col-md-10 mb-5">
+          <FavoriteList
+          
+          />
+        </div>
+       
+      </div>
+    </div></>
+  );
+};
+
+export default Profile;
+
+
+
+// const Profile = () => {
+//   return (
+//     <header>
+//     <Nav />
+//     <div className='container profile__container'>
+//     <h1>Your Crypto Tracker!</h1>
+//     <h5>Here are your favorite coins!</h5>
+//     <div className='favorites-list'>
+//       {/* <FavoriteList 
+        
+//         /> */}
+//     </div>
+
+//     </div>
+//     < CoinList/>
+//     </header>
+//   )
+// }
 
 // export default Profile
